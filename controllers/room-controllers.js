9a72bca860e659059a8245cec2e48950ff6be2c8
Darric Heng <77005864+darricheng@users.noreply.github.com>
2022-12-06 22:37:26 +0800

    Add logic for getting and booking rooms

--- controllers/room-controllers.js
+++ controllers/room-controllers.js
@@ -8,7 +8,9 @@ const httpStatus = require("http-status");
 // Controllers
 const findRooms = async (req, res) => {
   try {
+    // Query the database based on the query params
     const result = await Room.find(req.query);
+    // Return the resulting object as JSON
     res.json(result);
   } catch (e) {
     console.error(e);
@@ -17,7 +19,26 @@ const findRooms = async (req, res) => {
 };
 const bookRoom = async (req, res) => {
   try {
-    res.send("Still implementing");
+    // Extract the room type to a variable for easier handling
+    let roomType = req.params.roomType;
+
+    // Find the first document for the roomType that booked is false and update booked to true
+    const result = await Room.findOneAndUpdate(
+      { room_type: roomType, booked: false },
+      { booked: true }
+    ); // Returns null if no rooms are found
+    console.log(result);
+
+    // If no empty rooms are found, return the relevant httpStatus
+    if (!result) {
+      res
+        .status(httpStatus.NOT_FOUND)
+        .send(`No empty rooms found for request: ${roomType}`);
+      return;
+    }
+
+    // If a room was booked successfully, return http success
+    res.status(httpStatus.OK).send(`${roomType} booked`);
   } catch (e) {
     console.error(e);
     res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
