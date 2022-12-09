bf97eccf119049f1f6eb90066c728271a46acf35
Darric Heng <77005864+darricheng@users.noreply.github.com>
2022-12-09 21:06:44 +0800

    Add controllers for guest endpoint

--- controllers/guest-controllers.js
+++ controllers/guest-controllers.js
@@ -6,6 +6,52 @@ const { Guest } = require("../models");
 const httpStatus = require("http-status");
 
 // Controllers
+// Get guests
+const getGuests = async (req, res) => {
+  try {
+    const guests = await Guest.find(req.query);
+    res.status(httpStatus.OK).json(guests);
+  } catch (error) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
+  }
+};
+
+// Add a new guest
+const addGuest = async (req, res) => {
+  try {
+    const guest = await Guest.create(req.body);
+    res.status(httpStatus.CREATED).json(guest);
+  } catch (error) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
+  }
+};
+
+// Delete a guest
+const deleteGuest = async (req, res) => {
+  try {
+    const guestId = req.params.guestId;
+    const guest = await Guest.findByIdAndDelete(guestId);
+    res.status(httpStatus.OK).json(guest);
+  } catch (error) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
+  }
+};
+
+// Edit a guest
+const editGuest = async (req, res) => {
+  try {
+    const guestId = req.params.guestId;
+    const guest = await Guest.findByIdAndUpdate(guestId, req.body);
+    res.status(httpStatus.OK).json(guest); // Returns the old unedited guest
+  } catch (error) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
+  }
+};
 
 // Export functions
-module.exports = {};
+module.exports = {
+  getGuests,
+  addGuest,
+  deleteGuest,
+  editGuest,
+};
