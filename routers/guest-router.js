8d70de74149a1b3fc16dc1e2ca5628e127e9053d
Darric Heng <77005864+darricheng@users.noreply.github.com>
2022-12-09 21:06:16 +0800

    Add routers for the guest endpoint

--- routers/guest-router.js
+++ routers/guest-router.js
@@ -3,8 +3,17 @@ const express = require("express");
 const router = express.Router();
 
 // Controller imports
-const {} = require("../controllers/guest-controllers");
+const {
+  getGuests,
+  addGuest,
+  deleteGuest,
+  editGuest,
+} = require("../controllers/guest-controllers");
 
 // Endpoints
+router.get("/", getGuests);
+router.post("/add", addGuest);
+router.delete("/:guestId", deleteGuest);
+router.put("/:guestId", editGuest);
 
 module.exports = { router };
